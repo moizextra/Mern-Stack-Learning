751404e0f4e1d0b227b55b43ca46ad240d4953a3
nishantshenoy <nishantshenoy83@gmail.com>
2023-10-04 23:23:45 +0530

    Add asyncWrapper to Controllers

--- backend/middleware/catchAsyncError1.js
+++ backend/middleware/catchAsyncError1.js
@@ -1,3 +1,17 @@
-module.exports = asyncErrorHandle => (req, res, next) => {
-    Promise.resolve(asyncErrorHandle(req, res, next)).catch(next());
+const ErrorHander = require('../utils/errorhandler');
+
+// module.exports = asyncErrorHandle => (req, res, next) => {
+//     Promise.resolve(asyncErrorHandle(req, res, next)).catch(next());
+// };
+
+const asyncWrapper = asyncErrorHandle => {
+    return async (req, res, next) => {
+        try {
+            await asyncErrorHandle(req, res, next);
+        } catch (error) {
+            next(new ErrorHander(err.message, 500));
+        }
+    };
 };
+
+module.exports = asyncWrapper;
