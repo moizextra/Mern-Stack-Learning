dd84d433377bbe02ef1ce7ed9d3c3ed3ebbb38dc
moizsheraz <moiz77131@gmail.com>
2023-10-22 12:29:42 +0500

    feat:myOrders

--- frontend/src/CartSlices/index.js
+++ frontend/src/CartSlices/index.js
@@ -1,47 +1,50 @@
 import { createSlice } from '@reduxjs/toolkit';
+
 const initialState = {
-    cartItems: JSON.parse(localStorage.getItem('cartItems')) || [],
-    amount: 0,
-    total: JSON.parse(localStorage.getItem('Total')) || 0,
-    isLoading: true,
-    ShippingInfo:JSON.parse(localStorage.getItem("ShippingInfo")) || {}
-  };
-  const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-     addtoCart(state, action) {
-        const {name,Price,quantity,image,product} = action.payload; //product is actually id
-        const existingItem = state.cartItems.find(item => item.product === product);
-        if(existingItem){
-            existingItem.quantity += 1;
-       } else{
-        state.cartItems.push({name,Price,quantity,image,product});  
-       }
-       localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
-       state.total = state.cartItems.reduce((total, item) => {
-        return total + item.Price * item.quantity;  
+  cartItems: JSON.parse(localStorage.getItem('cartItems')) || [],
+  amount: 0,
+  total: 0,
+  isLoading: true,
+  ShippingInfo: JSON.parse(localStorage.getItem('ShippingInfo')) || {},
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addtoCart(state, action) {
+      const { name, price, quantity, image, product } = action.payload;
+      const existingItem = state.cartItems.find(item => item.product === product);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.cartItems.push({ name, price, quantity, image, product });
+      }
+      localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
+      state.total = state.cartItems.reduce((total, item) => {
+        return total + (parseFloat(item.price) * item.quantity);
       }, 0);
       localStorage.setItem('Total', JSON.stringify(state.total));
-     },
-     removeFromCart(state, action) {
-      const index = state.cartItems.findIndex(item => item.product === action.payload); // getting the id of the product
-      state.cartItems.splice(index, 1);  // at particular index and only one Element
-      localStorage.setItem('cartItems', JSON.stringify(state.cartItems)); 
+    },
+    removeFromCart(state, action) {
+      const productId = action.payload;
+      const index = state.cartItems.findIndex(item => item.product === productId);
+      if (index !== -1) {
+        state.cartItems.splice(index, 1);
+        localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
+      }
       state.total = state.cartItems.reduce((total, item) => {
-        return total + item.Price * item.quantity;
+        return total + (parseFloat(item.price) * item.quantity);
       }, 0);
       localStorage.setItem('Total', JSON.stringify(state.total));
     },
     decrementQuantity(state, action) {
       const productId = action.payload;
       const existingItem = state.cartItems.find(item => item.product === productId);
-
       if (existingItem) {
         if (existingItem.quantity > 1) {
           existingItem.quantity -= 1;
         } else {
-          // If quantity becomes 0 or negative, remove the item from the cart
           const index = state.cartItems.findIndex(item => item.product === productId);
           if (index !== -1) {
             state.cartItems.splice(index, 1);
@@ -50,17 +53,16 @@ const initialState = {
       }
       localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
       state.total = state.cartItems.reduce((total, item) => {
-        return total + item.Price * item.quantity;
+        return total + (parseFloat(item.price) * item.quantity);
       }, 0);
       localStorage.setItem('Total', JSON.stringify(state.total));
     },
-    SaveCartInfo(state,action) {
-localStorage.setItem("ShippingInfo",JSON.stringify(action.payload));
-state.ShippingInfo=JSON.parse(localStorage.getItem("ShippingInfo"));
-    }
+    SaveCartInfo(state, action) {
+      localStorage.setItem('ShippingInfo', JSON.stringify(action.payload));
+      state.ShippingInfo = JSON.parse(localStorage.getItem('ShippingInfo'));
+    },
+  },
+});
 
-    }
-  });
-  console.log(cartSlice);
-  export const { addtoCart,decrementQuantity,removeFromCart,SaveCartInfo} = cartSlice.actions;
+export const { addtoCart, decrementQuantity, removeFromCart, SaveCartInfo } = cartSlice.actions;
 export default cartSlice.reducer;
--- frontend/src/Pages/MyOrders.jsx
+++ frontend/src/Pages/MyOrders.jsx
@@ -1,68 +1,60 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { myOrders } from '../OrderSlices/myOrders';
-
+import { DataGrid} from '@mui/x-data-grid';
 const MyOrders = () => {
   const dispatch = useDispatch();
   const myorders = useSelector((state) => state.myOrders.myOrders);
-
+console.log(myorders);
   useEffect(() => {
     dispatch(myOrders());
   }, [dispatch]);
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'Name',
+      headerName: 'Name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'Status',
+      headerName: ' Payment Status',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'Price',
+      headerName: 'Price',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+   
+  ];
+  
 
   return (
     <>
-      {myorders.map((order) => (
-        <div
-          key={order.id} // Make sure to assign a unique key to each order
-          className={`bg-gray-100 min-h-screen flex items-center justify-center ${
-            order.OrderStatus === 'Completed' ? 'bg-green-100' : ''
-          }`}
-        >
-          <div className="bg-white shadow-md rounded-lg p-4  md:w-1/2 lg:w-1/3">
-            <h1 className="text-2xl font-semibold mb-4 text-blue-600">Orders</h1>
-            <div className="border-b border-gray-300 mb-4 pb-4">
-              <div className="flex items-center justify-between mb-2">
-                <div>
-                  <p className="text-gray-600">
-                    <strong>Status:</strong> {order.OrderStatus}
-                  </p>
-                  <p className="text-gray-600">
-                    <strong>Price:</strong> ${order.ItemsPrice}
-                  </p>
-                </div>
-                <img
-                  src="status_icon.png"
-                  alt="Status Icon"
-                  className="w-6 h-6"
-                />
-              </div>
-            </div>
-            <div className="text-gray-600">
-              <p>
-                <strong>Payment ID:</strong> {order.paymentInfo.id}
-              </p>
-              <p>
-                <strong>Created At:</strong> {order.createdAt}
-              </p>
-              <p>
-                <strong>Paid At:</strong> {order.paidAt}
-              </p>
-            </div>
-            <div className=" text-gray-600">
-              <p>
-                <strong>Shipping Address:</strong>
-              </p>
-              <address>
-                {order.shippinginfo.address}
-                <br />
-                {order.shippinginfo.city}, {order.shippinginfo.state},{' '}
-                {order.shippinginfo.country}
-              </address>
-            </div>
-          </div>
-        </div>
-      ))}
+    <DataGrid
+         rows={myorders.map((order) => ({
+          id: order._id,
+          Name: order.OrderItems[0].name,
+          Status: order.paymentInfo.status,
+          Price: order.ItemsPrice,
+        }))}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              PriceSize: 5,
+            },
+          },
+        }}
+        pPriceSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
     </>
   );
 };
